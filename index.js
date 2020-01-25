6cec918213eeed694021bd503755a78b9819add9
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2020-01-25 13:42:38 +0100

    Finish responsiveness

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const showMore = document.querySelector(".show-more");
-const description = document.querySelector(".more-less-description");
-showMore.addEventListener('click', handleShowMore)
 
-function handleShowMore(e){
+const handleShowMore = (e) => {
+  const description = document.querySelector(".more-less-description");
   description.classList.toggle('auto-height');
 
   if (showMore.innerHTML === 'Show more') {
     showMore.innerHTML = 'Show Less';
     return;
   } 
-
+  
   showMore.innerHTML = 'Show more'; 
-}
+};
+
+const handleSearchBar = () => {
+  const searchHolder = document.querySelector('.mobile-search-holder');
+  searchHolder.classList.toggle('open-search');
+};
+
+const showMore = document.querySelector(".show-more");
+showMore.addEventListener('click', handleShowMore);
+
+const mobileSearchIcon = document.querySelector('.mobile-search-icon');
+mobileSearchIcon.addEventListener('click', handleSearchBar);
+
+const closeSearchIcon = document.querySelector('.close-search-icon');
+closeSearchIcon.addEventListener('click', handleSearchBar);
